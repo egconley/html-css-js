9e0c0a36c348c42ab697d40dd29998c7258c41dc
egconley <31393715+egconley@users.noreply.github.com>
2019-09-19 16:04:18 -0700

    added loops and functions

--- get-userName.js
+++
@@ -1,4 +0,0 @@
-var userName = prompt('What\'s your name?');
-
-document.write('<h3>' + 'Hi ' + userName + '!' + '</h3>');
-
---
+++ js/app.js
@@ -0,0 +1,24 @@
+function askName() {
+    var userName = prompt('What\'s your name?');
+    return 'Hi ' + userName + '!';
+}
+
+function likeDogs() {
+    var askAboutDogs = confirm('Are you a dog that likes dogs?');
+    var dog = '<img src="https://cdn1.iconfinder.com/data/icons/hand-drawn/100/handdrawn_pup-512.png">';
+    if (askAboutDogs === true) {
+        var howMany = '';
+        while (Number.isInteger(howMany) === false) {
+            howMany = Number(prompt('How many dogs are you interested in connecting with?'));
+        }
+            var result = '';
+            for (i=0; i < (howMany); i++) {
+                count = i + 1;
+                result = '<div>' + result + count + dog + '</div>';  
+            }
+            return result; 
+    }
+    else {
+        return 'You may want to check out our sister site, Cat Tinder.';
+    } 
+}
